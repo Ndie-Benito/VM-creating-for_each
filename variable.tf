98fecd7139456af478b17c7b61ad896a7cef8160
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-02-10 20:20:19 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -11,3 +11,23 @@ variable "client_secret"{
 variable "tenant_id"{
   type=string
 }
+variable "vms" {
+  description = "Configuration des machines virtuelles"
+  type = map(object({
+    vm_size      = string
+    admin_username = string
+    admin_password = string
+  }))
+  default = {
+    "vm1" = {
+      vm_size      = "Standard_B1s"
+      admin_username = "adminuser"
+      admin_password = "P@ssw0rd123!"
+    },
+    "vm2" = {
+      vm_size      = "Standard_B1s"
+      admin_username = var.admin_username
+      admin_password = var.admin_password
+    }
+  }
+}
