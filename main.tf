c2f4cdd48d694f2bc17aff1ba688e8332b90eaed
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-02-10 20:18:56 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,57 @@
+# Crée un réseau virtuel
+resource "azurerm_virtual_network" "vnet" {
+  name                = "my-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Crée un sous-réseau
+resource "azurerm_subnet" "subnet" {
+  name                 = "my-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Crée une interface réseau pour chaque VM
+resource "azurerm_network_interface" "nic" {
+  for_each = var.vms
+
+  name                = "${each.key}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Crée les machines virtuelles
+resource "azurerm_linux_virtual_machine" "vm" {
+  for_each = var.vms
+
+  name                = each.key
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = each.value.vm_size
+  admin_username      = each.value.admin_username
+  admin_password      = each.value.admin_password
+  network_interface_ids = [azurerm_network_interface.nic[each.key].id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  disable_password_authentication = false
+}
